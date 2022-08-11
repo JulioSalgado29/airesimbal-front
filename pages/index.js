ca4a618735055905a4fabf1090c6fdea026e8eec
Julio Salgado <juliosalgado2998@hotmail.com>
2022-08-11 16:15:31 +0200

    prueba

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Home - Index2</h1>
+        <h1>Home - Index(main)</h1>
       </main>
 
       <footer className={styles.footer}>
