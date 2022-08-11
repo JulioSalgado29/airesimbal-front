ea61f52dc334d55253a5c8ab3a4a796c34476f41
Julio Salgado <juliosalgado2998@hotmail.com>
2022-08-12 01:13:19 +0200

    try 1

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Probando Julio</h1>
+      <h1>Probando Julio2</h1>
     </div>
   )
 }
