8508f8a3ffb87de07e6bf974da1678c3640e2845
Julio Salgado <juliosalgado2998@hotmail.com>
2022-09-05 17:49:36 +0200

    prueba

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="home">
       <h1>Probando Julio212243</h1>
-      <h2>Probando Gregor2y</h2>
+      <h2>Probando Gregor2333y</h2>
       <h2>Probando Llama</h2>
     </div>
   )
